0a18f8dfc150ed93e4a3f29a01a4d8cc3fcb8e4d
leogtzr <leogutierrezramirez@gmail.com>
2019-08-31 17:44:11 -0600

    project skeleton

---
+++ java/book-reading-path/src/main/java/com/book/path/BookReadingPathApplication.java
@@ -0,0 +1,13 @@
+package com.book.path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReadingPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReadingPathApplication.class, args);
+	}
+
+}
